c9a4a1982ac4e45f9adfbd4ab364efdf31937535
Mark Butcher <mark.butcher@puppetlabs.com>
2014-11-18 15:35:46 -0800

    fixing a low permgen warning in scripts/sbt.sh

--- jenkins-integration/scripts/sbt.sh
+++ jenkins-integration/scripts/sbt.sh
@@ -8,6 +8,7 @@ SBTPATH=$4
 export PUPPET_GATLING_SIMULATION_ID=$SIM_ID
 export PUPPET_GATLING_MASTER_BASE_URL=https://$MASTER:8140
 export PUPPET_GATLING_SIMULATION_CONFIG=./config/scenarios/$SCENARIO
+export SBT_OPTS="-Xms512M -Xmx1536M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M"
 
 cd ../simulation-runner
-java -Xss2M -Dsbt.log.noformat=true -jar $SBTPATH run
+java $SBT_OPTS -Xss2M -Dsbt.log.noformat=true -jar $SBTPATH run
